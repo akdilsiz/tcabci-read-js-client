e990cfca1f8f6bb261b023e248888cb66196a17a
Abdulkadir DILSIZ <abdulkadir.dilsiz@transferchain.io>
2026-05-04 16:58:34 +0300

    Version bumps

--- client.js
+++ client.js
@@ -57,7 +57,7 @@ export default class TCaBCIClient {
   _connected = false
   _chainName = 'transferchain'
   _chainVersion = 'v1'
-  _version = `v2.6.6`
+  _version = `v2.7.0`
   /**
    * @type {?SuccessCallback}
    */
