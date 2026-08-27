df23c6764f789f8b30b3d2933066c62b72ab86ca
Abdulkadir D <akdilsiz@tecpor.com>
2026-08-27 03:34:51 +0300

    Version bumps

--- websocketOptions.js
+++ websocketOptions.js
@@ -15,7 +15,7 @@ export class Options {
   _protocols = []
   _customWS
   _endpoints = ['ws', 'longpoll']
-  _version = `v2.7.8`
+  _version = `v2.7.9`
 
   _maxConnectionDelay = 10000
   _minReconnectionDelay = 1000 + Math.random() * 4000
